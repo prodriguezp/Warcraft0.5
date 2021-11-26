c7b317050b95bbb296eff8c0e38906e6b4aa2f44
Pedro Luis <pedroluisrp1998@gmail.com>
2021-11-26 16:14:18 +0100

    Turnos

--- cmake-build-debug/CMakeFiles/Warcraft0_5.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Warcraft0_5.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/wafac/CLionProjects/Warcraft0.5/main.cpp" "C:/Users/wafac/CLionProjects/Warcraft0.5/cmake-build-debug/CMakeFiles/Warcraft0_5.dir/main.cpp.obj"
+  "C:/Users/wafac/Documents/CPRojects/Warcraft0.5/main.cpp" "C:/Users/wafac/Documents/CPRojects/Warcraft0.5/cmake-build-debug/CMakeFiles/Warcraft0_5.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "MSVC")
 
--- main.cpp
+++ main.cpp
@@ -15,44 +15,109 @@ public:
     bool estaVivo();
     Personaje();
     Personaje(std::string nombre);
+
+    const std::string &getNombre() const;
+
+    int getVida() const;
+
+    int getFuerza() const;
+
+    int getDefensa() const;
 };
 
 void Personaje::pintarInfo() {
-    std::cout << "Nombre:  "+this->nombre << std::endl;
+    std::cout << "##############################"<< std::endl;
+    std::cout << "-----------------------"<< std::endl;
+    std::cout << "|" << std::endl;
+    std::cout << "|"+this->nombre << std::endl;
+    std::cout << "|" << std::endl;
+    std::cout << "-----------------------"<< std::endl;
     std::cout << "Vida:    "<< this->vida << std::endl;
     std::cout << "Defensa: "<<this->defensa << std::endl;
     std::cout << "Fuerza:  "<<this->fuerza << std::endl;
+    std::cout << "##############################"<< std::endl;
 }
 Personaje::Personaje(std::string nombre) {
     this->nombre = nombre;
-    this->vida=0;
-    this->fuerza=0;
+    this->vida=100;
+    this->fuerza=10;
     this->defensa=0;
 }
 
 bool Personaje::estaVivo() {
-    return this->vida<=0;
+    return this->vida>0;
 }
 int Personaje::atacar(Personaje* otro) {
     int danioRecibido = 0;
     if(rand()%4 == 0){
         danioRecibido = this->fuerza - otro->defensa;
-        otro->vida - danioRecibido;
+        otro->vida -=  danioRecibido;
     }
     return danioRecibido;
 }
-int main() {
-    std::string nombre;
 
+
+const std::string &Personaje::getNombre() const {
+    return nombre;
+}
+
+int Personaje::getVida() const {
+    return vida;
+}
+
+int Personaje::getFuerza() const {
+    return fuerza;
+}
+
+int Personaje::getDefensa() const {
+    return defensa;
+}
+
+void turno(Personaje* personaje1,Personaje* personaje2);
+
+int main() {
+    srand (time(NULL));
+    std::string nombre;
+    std::string candenaAux;
     printf("Introduce el nombre del jugador: ");
     std::getline(std::cin,nombre);
 
 
     Personaje p1 =  {nombre};
-    Personaje p2 =  {"Juan"};
+    Personaje p2 =  {"Orco del vacio"};
 
+
+    //p1.pintarInfo();
+    //p2.pintarInfo();
+
+    while (p1.estaVivo() && p2.estaVivo()){
+        printf("Pulsa cualquier la tecla Enter para luchar...\n");
+        std::getline(std::cin,candenaAux);
+        std::cout << "=========================================" << std::endl;
+        std::cout << "=========================================" << std::endl;
+        turno(&p1,&p2);
+        turno(&p2,&p1);
+        std::cout << "=========================================" << std::endl;
+        std::cout << "=========================================" << std::endl;
+
+
+    }
 
 
 
     return 0;
 }
+
+void turno(Personaje* p1,Personaje* p2){
+    int danio = 0;
+    std::cout << p1->getNombre() << " se  dispone a atacar a " << p2->getNombre() << std::endl;
+    danio = p1->atacar(p2);
+
+    if(danio > 0){
+        std::cout << "     OK:::: El ataque acerto, dejando a " << p2->getNombre() << " con " << std::to_string(p2->getVida()) << "de vida" << std::endl;
+    }
+    else{
+        std::cout << "     FAIL:::: El ataque no acerto..." << std::endl;
+    }
+}
+
